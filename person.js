5c46ad1058ac65db0874f555f02a55cd5a529b77
smkisvarday <90298147+smkisvarday@users.noreply.github.com>
2021-12-08 22:30:45 -0500

    Add files via upload

---
+++ person.js
@@ -0,0 +1,17 @@
+// const person = {
+//     name: "John Doe",
+//     age: 30
+// }
+
+console.log(__dirname, __filename);
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    greeting () {
+       console.log(`My name is ${this.name} and my age is ${this.age}`);
+    }
+}
+module.exports = Person;
